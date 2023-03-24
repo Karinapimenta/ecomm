1c83ebcd47982e34e880c946c51dc8e742c3573e
Karina Pimenta <karinabpimenta@gmail.com>
2023-03-24 12:57:18 -0300

    fix e reparação em arquivo de conexão

--- product/src/config/dbConnect.js
+++ product/src/config/dbConnect.js
@@ -3,11 +3,10 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 const DB_HOST = process.env.NODE_ENV === 'test' ? '127.0.0.1' : process.env.DB_HOST;
-const { USER, PASSWORD, ACCESSPORT } = process.env;
 
 mongoose.set('strictQuery', false);
 mongoose.connect(
-  `mongodb://${USER}:${PASSWORD}@${DB_HOST}:${ACCESSPORT}/ecomm-product?authSource=admin`,
+  `mongodb://admin:secret@${DB_HOST}:27017/ecomm-product?authSource=admin`,
 );
 const db = mongoose.connection;
 export default db;
